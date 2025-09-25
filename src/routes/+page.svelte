62aec6a69ff6bd5b6d3330995e59f4f7f77e1913
MojoNojo06 <azaleah.j06@gmail.com>
2025-09-25 15:44:35 -0500

    basic HP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <h1>The Deck</h1>
-<button on:click={printToLog}>Log all cards</button>
-<button on:click={populate}>Populate Deck</button>
-<button on:click={shuffle}>Shuffle the Deck</button>
-<button on:click={clear}>Clear the Deck</button>
+<p>HP: {health}</p>
+<br>
+<button onclick={printToLog}>Log all cards</button>
+<button onclick={populate}>Populate Deck</button>
+<button onclick={shuffle}>Shuffle the Deck</button>
+<button onclick={clear}>Clear the Deck</button>
+<br>
+<button onclick={() => health++}>Increment HP</button>
+<button onclick={() => health--}>Decrement HP</button>
 
 <script lang="ts">
     import { populate, shuffle, printToLog, clear } from './scoundrel';
+    let health: number = $state(20)
 </script>
