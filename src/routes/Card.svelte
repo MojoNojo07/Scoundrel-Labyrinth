30cbafae853a6260076025423a022036d47b0183
MojoNojo07 <azaleah.j06@gmail.com>
2025-12-13 14:18:59 -0600

    card animation, starting click and drag

--- src/routes/Card.svelte
+++ src/routes/Card.svelte
@@ -1,4 +1,4 @@
-<div class="card">
+<div class="card" onmousedown={clickLog}>
     {card_id} <br>
     {card.toString()}
 
@@ -11,9 +11,19 @@
         border: 5px solid white;
         width:200px;
         margin-left:50px;
+        transition: transform 0.1s;
+        text-align: center;
+    }
+
+    .card:hover {
+        transform:translateY(-10px);
     }
 </style>
 
 <script>
     let { card_id, card } = $props();
+
+    function clickLog() {
+        console.log("Clicked " + card.toString() + " with ID " + card_id);
+    }
 </script>
